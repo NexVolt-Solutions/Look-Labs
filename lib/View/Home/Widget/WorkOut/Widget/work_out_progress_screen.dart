ae6e8a76c7e52a6aa82722e5fdde315f2de3e5b5
Muhammad Shehzad <127086042+Muhammd-Shehzad@users.noreply.github.com>
2026-02-10 16:02:16 +0500

    missing widget completed

--- lib/View/Home/Widget/WorkOut/Widget/work_out_progress_screen.dart
+++ lib/View/Home/Widget/WorkOut/Widget/work_out_progress_screen.dart
@@ -6,6 +6,7 @@ import 'package:looklabs/Core/Widget/app_bar_container.dart';
 import 'package:looklabs/Core/Widget/custom_container.dart';
 import 'package:looklabs/Core/Widget/height_widget_cont.dart';
 import 'package:looklabs/Core/Widget/line_chart_widget.dart';
+import 'package:looklabs/Core/Widget/linear_slider_widget.dart';
 import 'package:looklabs/Core/Widget/normal_text.dart';
 import 'package:looklabs/Core/Widget/plan_container.dart';
 import 'package:looklabs/Core/Constants/app_assets.dart';
@@ -150,8 +151,47 @@ class _WorkOutProgressScreenState extends State<WorkOutProgressScreen> {
                 ),
               ],
             ),
-            SizedBox(height: context.h(16)),
+            SizedBox(height: context.h(8)),
+            SizedBox(
+              height: context.h(130), // ✅ SAFE HEIGHT
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                padding: EdgeInsets.zero,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(right: context.w(12)),
+                    child: SizedBox(
+                      width: context.w(220),
+                      child: PlanContainer(
+                        padding: context.padSym(h: 7, v: 24),
+                        isSelected: false,
+                        onTap: () {},
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            NormalText(
+                              titleText: 'Fitness Consistency',
+                              titleSize: context.text(16),
+                              titleWeight: FontWeight.w500,
+                              titleColor: AppColors.subHeadingColor,
+                            ),
+                            SizedBox(height: context.h(12)),
+                            LinearSliderWidget(
+                              progress: 10,
+                              height: context.h(12),
+                              animatedConHeight: context.h(12),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
 
+            SizedBox(height: context.h(8)),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: List.generate(
@@ -171,8 +211,7 @@ class _WorkOutProgressScreenState extends State<WorkOutProgressScreen> {
                     border: isSelected
                         ? Border.all(color: AppColors.pimaryColor, width: 1.5)
                         : null,
-                    padding: context.padSym(h: 37, v: 13),
-                    margin: EdgeInsets.only(right: 8),
+                    padding: context.padSym(h: 42, v: 12),
                     child: Center(
                       child: Text(
                         yourProgressScreenViewModel.buttonName[index],
@@ -187,6 +226,42 @@ class _WorkOutProgressScreenState extends State<WorkOutProgressScreen> {
                 },
               ),
             ),
+
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //   children: List.generate(
+            //     yourProgressScreenViewModel.buttonName.length,
+            //     (index) {
+            //       final bool isSelected =
+            //           yourProgressScreenViewModel.selectedIndex ==
+            //           yourProgressScreenViewModel.buttonName[index];
+            //       return CustomContainer(
+            //         radius: context.radius(10),
+            //         onTap: () {
+            //           yourProgressScreenViewModel.selectIndex(index);
+            //         },
+            //         color: isSelected
+            //             ? AppColors.buttonColor.withOpacity(0.11)
+            //             : AppColors.backGroundColor,
+            //         border: isSelected
+            //             ? Border.all(color: AppColors.pimaryColor, width: 1.5)
+            //             : null,
+            //         padding: context.padSym(h: 37, v: 13),
+            //         margin: EdgeInsets.only(right: 8),
+            //         child: Center(
+            //           child: Text(
+            //             yourProgressScreenViewModel.buttonName[index],
+            //             style: TextStyle(
+            //               fontSize: context.text(14),
+            //               fontWeight: FontWeight.w700,
+            //               color: AppColors.seconderyColor,
+            //             ),
+            //           ),
+            //         ),
+            //       );
+            //     },
+            //   ),
+            // ),
             SizedBox(height: context.h(16)),
             CustomContainer(
               radius: context.radius(10),
