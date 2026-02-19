3250354124bafbec56630b1552268c0af1614db4
Muhammad Shehzad <127086042+Muhammd-Shehzad@users.noreply.github.com>
2026-02-19 15:17:01 +0500

    i add banner app bar widget updated code

--- lib/features/Widget/app_bar_container.dart
+++ lib/features/Widget/app_bar_container.dart
@@ -98,3 +98,110 @@ class AppBarContainer extends StatelessWidget {
     );
   }
 }
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:looklabs/Features/Widget/normal_text.dart';
+// import 'package:looklabs/Core/Constants/app_assets.dart';
+// import 'package:looklabs/Core/Constants/app_colors.dart';
+// import 'package:looklabs/Core/Constants/size_extension.dart';
+
+// class AppBarContainer extends StatelessWidget {
+//   final String title;
+//   final VoidCallback? onBackTap;
+//   final bool showBack;
+//   final bool showHeart;
+//   final VoidCallback? onHeartTap;
+
+//   const AppBarContainer({
+//     super.key,
+//     required this.title,
+//     this.onBackTap,
+//     this.showBack = false,
+//     this.showHeart = false,
+//     this.onHeartTap,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: EdgeInsets.only(top: context.h(8)),
+//       child: Row(
+//         children: [
+//           /// ⬅ Back OR Empty Space
+//           showBack
+//               ? GestureDetector(
+//                   onTap: onBackTap,
+//                   child: Container(
+//                     height: context.h(40),
+//                     width: context.w(40),
+//                     decoration: BoxDecoration(
+//                       color: AppColors.backGroundColor,
+//                       borderRadius:
+//                           BorderRadius.circular(context.radius(12)),
+//                       border: Border.all(
+//                         color: AppColors.white.withOpacity(0.2),
+//                         width: 1.5,
+//                       ),
+//                       boxShadow: [
+//                         BoxShadow(
+//                           color: AppColors.arrowBlurColor,
+//                           offset: const Offset(5, 5),
+//                           blurRadius: 5,
+//                         ),
+//                         BoxShadow(
+//                           color: AppColors.customContinerColorDown,
+//                           offset: const Offset(-5, -5),
+//                           blurRadius: 30,
+//                         ),
+//                       ],
+//                     ),
+//                     child: Center(
+//                       child: SvgPicture.asset(
+//                         AppAssets.backIcon,
+//                         fit: BoxFit.scaleDown,
+//                         placeholderBuilder: (_) => const Icon(
+//                           Icons.arrow_back,
+//                           color: Colors.white,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 )
+//               : SizedBox(
+//                   height: context.h(40),
+//                   width: context.w(40),
+//                 ),
+
+//           /// 🏷 Title (Perfectly centered)
+//           Expanded(
+//             child: Center(
+//               child: NormalText(
+//                 titleText: title,
+//                 titleSize: context.text(20),
+//                 titleWeight: FontWeight.w600,
+//                 titleColor: AppColors.headingColor,
+//                 maxLines: 1,
+//                 overflow: TextOverflow.ellipsis,
+//               ),
+//             ),
+//           ),
+
+//           /// ❤️ Heart OR Empty Space
+//           SizedBox(
+//             height: context.h(40),
+//             width: context.w(40),
+//             child: showHeart
+//                 ? GestureDetector(
+//                     onTap: onHeartTap,
+//                     child: SvgPicture.asset(
+//                       AppAssets.appaleIcon,
+//                       fit: BoxFit.scaleDown,
+//                     ),
+//                   )
+//                 : const SizedBox(),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
